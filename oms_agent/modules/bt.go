63c9f23f0277bf042d2878e895d6937e843dcc01
dylan <dylanliu120@163.com>
2019-03-16 11:49:30 +0800

    new

--- oms_agent/modules/bt.go
+++ oms_agent/modules/bt.go
@@ -170,7 +170,7 @@ func Download(step utils.Step, procDir string, resultChannel chan string, status
 	clientConfig := torrent.NewDefaultClientConfig()
 	t := step.FileParam[0]
 	log.Println(t)
-	torrentStream := []byte(step.FileParam[0][:].(string))
+	torrentStream := []byte(step.FileParam[0].(string)[:])
 	torrentPath := filepath.Join("/tmp", strings.Join([]string{step.InstanceID, "torrent"}, "."))
 	f, err := os.OpenFile(torrentPath, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0400)
 	defer f.Close()
