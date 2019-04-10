0bdd3d2fcdcdbd35bbcadd8ddb5982843aeae8f2
dylan <dylanliu120@163.com>
2019-04-10 15:05:17 +0800

    new

--- oms_agent/btgo/bencode/encode.go
+++ oms_agent/btgo/bencode/encode.go
@@ -44,6 +44,32 @@ func (e *Encoder) write(s string) {
 
 }
 
+// Returns whether the value represents the empty value for its type. Used for
+// example to determine if complex types satisfy the common "omitempty" tag
+// option for marshalling. Taken from
+// http://stackoverflow.com/a/23555352/149482.
+func IsEmptyValue(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Func, reflect.Map, reflect.Slice:
+		return v.IsNil()
+	case reflect.Array:
+		z := true
+		for i := 0; i < v.Len(); i++ {
+			z = z && IsEmptyValue(v.Index(i))
+		}
+		return z
+	case reflect.Struct:
+		z := true
+		for i := 0; i < v.NumField(); i++ {
+			z = z && IsEmptyValue(v.Field(i))
+		}
+		return z
+	}
+	// Compare other types directly:
+	z := reflect.Zero(v.Type())
+	return v.Interface() == z.Interface()
+}
+
 func (e *Encoder) encode(v reflect.Value) (err error) {
 	switch v.Kind() {
 	case reflect.String:
@@ -105,7 +131,7 @@ func (e *Encoder) encode(v reflect.Value) (err error) {
 			sort.Sort(s)
 			for _, val := range s {
 				log.Info(isNilValue(val.value))
-				if val.omit_empty && isNilValue(val.value) {
+				if val.omit_empty && IsEmptyValue(val.value) {
 					continue
 				}
 				e.write(fmt.Sprintf("%d:%s", len(val.key), val.key))
