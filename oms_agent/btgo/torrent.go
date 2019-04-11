78b7cf3755df3fd07cc090d397af45bed9efae30
dylan <dylanliu120@163.com>
2019-04-11 16:12:15 +0800

    new

--- oms_agent/btgo/torrent.go
+++ oms_agent/btgo/torrent.go
@@ -47,34 +47,27 @@ func (info *Info) GenPieces(files []File) {
 		path := append([]string{"/"}, f.Path...)
 		fi, err := os.Open(
 			filepath.Join(path...))
-		//pr, pw := io.Pipe()
-		//wn, err := io.CopyN(pw, fi, f.Length)
 		fi.Close()
 
 		buf := make([]byte, info.PieceLength)
 		if !utils.CheckError(err) {
 			for {
 				h := sha1.New()
-				//file.Seek(info.PieceLength-1, 0)
 				n, err := fi.Read(buf)
-
 				if err != nil {
 					if err == io.EOF {
 						break
 					}
 				}
 				h.Write(buf)
-				//info.Pieces = h.Sum(info.Pieces)
 				pieces = h.Sum(pieces)
-				//log.Info(pieces)
+				log.Println(n)
 				if int64(n) < info.PieceLength {
 					break
 				}
 			}
-
 		}
 	}
-	//log.Println(string(pieces))
 	info.Pieces = pieces
 
 }
@@ -87,14 +80,11 @@ func NewTorrent(jid string, files []string) (t *Torrent) {
 		Comment:      "",
 		CreatedBy:    "go agent",
 		CreationDate: time.Now().Unix()}
-	//var pieces []byte
 	for _, f := range files {
 		fi, err := os.Stat(f)
 		if !utils.CheckError(err) {
 			switch mode := fi.Mode(); {
 			case mode.IsRegular():
-				//dirName := filepath.Dir(f)
-				//fileName := filepath.Base(f)
 				relPath, err := filepath.Rel("/", f)
 				if !utils.CheckError(err) {
 					metaInfo.Info.Files = append(
@@ -102,17 +92,14 @@ func NewTorrent(jid string, files []string) (t *Torrent) {
 						File{
 							Length: fi.Size(),
 							Path:   strings.Split(relPath, string(filepath.Separator))})
-					//pieces = append(pieces, metaInfo.Info.GenPieces(f)...)
 					metaInfo.Info.GenPieces(metaInfo.Info.Files)
 				}
 
 			default:
 				fmt.Println("directory")
-				//TODO
 			}
 		}
 	}
-	//metaInfo.Info.Pieces = pieces
 	s, _ := json.Marshal(metaInfo)
 	log.Infof("%s", s)
 	x, _ := bencode.Marshal(metaInfo)
