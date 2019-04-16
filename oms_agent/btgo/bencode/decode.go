769c3a76aa42a4f5805961a7a5a2fbaf7944a3e4
dylan <dylanliu120@163.com>
2019-04-16 20:36:35 +0800

    new

--- oms_agent/btgo/bencode/decode.go
+++ oms_agent/btgo/bencode/decode.go
@@ -43,6 +43,13 @@ func (d *Decoder) readNBytes(n int) (r []byte, err error) {
 	return
 }
 
+func (d *Decoder) readFull(b []byte) ([]byte, error) {
+	n, err := io.ReadFull(d.r, b)
+	if err != nil {
+
+	}
+}
+
 func (d *Decoder) decodeList(v reflect.Value) (err error) {
 	return
 }
@@ -60,29 +67,39 @@ func (d *Decoder) decodeString(v reflect.Value) (err error) {
 	length, err := strconv.ParseInt(string(data), 10, 0)
 	d.readNBytes(int(length))
 	data = data[1:]
+
+	switch v.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		err = d.decodeInt(v)
+	case reflect.String:
+		v.SetString(data)
+	case reflect.Slice, reflect.Array:
+		err = d.decodeList(v)
+	case reflect.Map:
+		err = d.decodeDict(v)
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			field := v.Field(i)
+			err = d.decode(reflect.ValueOf(field))
+		}
+	}
+
 	return
 }
 
 func (d *Decoder) decodeDict(v reflect.Value) (err error) {
-	//switch v.Kind(){
-	//case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-	//	err = d.decodeInt(v)
-	//case reflect.String:
-	//	err = d.decodeString(v)
-	//case reflect.Slice,reflect.Array:
-	//	err = d.decodeList(v)
-	//case reflect.Map:
-	//	err = d.decodeDict(v)
-	//case reflect.Struct:
-	//	for i := 0; i < v.NumField(); i++ {
-	//		field := v.Field(i)
-	//		err = d.decode(reflect.ValueOf(field))
-	//	}
-	//}
-	d.r.ReadByte()
+	b, err := d.r.ReadByte()
+	if err != nil {
+		if b == 'e' {
+			_, err := d.r.ReadByte()
+			return err
+		}
+	}
 	for {
-		v.F
-		d.decodeString(v)
+		err = d.decodeString(v)
+		if err != nil {
+			return err
+		}
 	}
 	return
 }
